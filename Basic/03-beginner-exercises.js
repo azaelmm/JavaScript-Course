1e2637730a5a54e85a3df12d5d4de44a6193fc6c
Azael Morell <azaelmorell15@gmail.com>
2024-12-11 09:44:12 +0100

    commit 03-beginner-exercises.js

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -9,3 +9,82 @@ lineas */
 
 // Ejercicio 3
 
+let string = "pepe"
+let int = 12
+let boolean = true
+let nullValue = null
+let BigInt = 71872816286186281628555555555555555555555555555561862862861868n
+let symbol = Symbol("symbol1")
+
+// Ejercicio 4
+
+console.log(string)
+console.log(int)
+console.log(boolean)
+console.log(nullValue)
+console.log(BigInt)
+console.log(symbol)
+
+// Ejercicio 5
+
+console.log(typeof string)
+console.log(typeof int)
+console.log(typeof boolean)
+console.log(typeof nullValue)
+console.log(typeof BigInt)
+console.log(typeof symbol)
+
+// Ejercicio 6
+
+string = "manolo"
+int = 16
+boolean = false
+nullValue = null
+BigInt = 222222222222222222222222222222222222222222222222222222222222n
+symbol = Symbol("symbol1999")
+
+console.log(string)
+console.log(int)
+console.log(boolean)
+console.log(nullValue)
+console.log(BigInt)
+console.log(symbol)
+
+
+// Ejercicio 7
+
+string = 5845
+int = "oscar"
+boolean = null
+nullValue = true
+BigInt = 3
+symbol = "pepe"
+
+console.log(string)
+console.log(int)
+console.log(boolean)
+console.log(nullValue)
+console.log(BigInt)
+console.log(symbol)
+
+// Ejerccicio 8
+
+const string2 = "pepe"
+const int2 = 12
+const boolean2 = true
+const nullValue2 = null
+const BigInt2 = 71872816286186281628555555555555555555555555555561862862861868n
+const symbol2 = Symbol("symbol1")
+
+// Ejercicio 9 y //Ejercicio 10
+
+// string2 = "pepe44"
+// int2 = 142
+// boolean2 = true
+// nullValue2 = null
+// BigInt2 = 11111111111111111111111111111111111111111111111111111111111111111111n
+// symbol2 = Symbol("symbol333")
+
+
+
+
