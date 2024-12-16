694e676c7b2df530ced61e684a37dc0cb0e2443f
Azael Morell <azaelmorell15@gmail.com>
2024-12-16 13:12:08 +0100

    commit clase Operators

---
+++ Basic/04-operators.js
@@ -0,0 +1,38 @@
+
+let a = 10
+let b = 5
+
+console.log (a+b)
+console.log (a-b)
+console.log (a*b)
+console.log (a/b)
+
+console.log(a%b) //modulo
+console.log(a ** b) // exponenete
+
+a++ // incremento
+console.log(a)
+
+b-- // decremento
+console.log(b)
+
+
+let myValue = 2
+myValue +=2
+console.log("MyValue ("+typeof(myValue)+"): "+myValue)
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a == b)
+console.log(a == 6)
+console.log(a == "6")
+console.log(a === a) //igualdad por identidad (por tipo y valor)
+console.log(a != "6")
+console.log(a !== "6")
+console.log(0 == false)
+console.log(1 == false)
+
+console.log(a == "6" && b < 20)
+console.log(a == "6" || b < 20)
